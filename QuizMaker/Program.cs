291aac0c4fa085def3b9386588c942714b75d777
Brenda Bertrand <brenda.bertrand.b@gmail.com>
2024-04-20 20:05:13 +0200

    Creation of QuestionAndAnswers class

--- QuizMaker/Program.cs
+++ QuizMaker/Program.cs
@@ -2,27 +2,8 @@
 
 class Program
 {
- 
- 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-      
     }
 }
---
+++ QuizMaker/QuestionAndAnswers.cs
@@ -0,0 +1,8 @@
+namespace QuizMaker;
+
+public class QuestionAndAnswers
+{
+    public string Question;
+    public List<string> AnswersList = new List<string>();
+    public List<int> CorrectAnswersIndex = new List<int>();
+}
