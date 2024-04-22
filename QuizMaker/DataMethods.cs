16d47c70a1e58ffe64260a449306f8a54866f3f6
Brenda Bertrand <brenda.bertrand.b@gmail.com>
2024-04-22 20:38:28 +0200

    Remove try/catch from ReadFromFile()

--- QuizMaker/DataMethods.cs
+++ QuizMaker/DataMethods.cs
@@ -5,28 +5,23 @@ namespace QuizMaker;
 public class DataMethods
 {
     static readonly XmlSerializer serializer = new XmlSerializer(typeof(List<QuestionAndAnswers>));
+
     public static void SaveToFile(List<QuestionAndAnswers> questionsList)
     {
-        using (FileStream file = File.Create(Constants.PATH_XML) )
+        using (FileStream file = File.Create(Constants.PATH_XML))
         {
             serializer.Serialize(file, questionsList);
         }
     }
+
     public static List<QuestionAndAnswers> ReadFromFile()
     {
         List<QuestionAndAnswers> questionsList = new List<QuestionAndAnswers>();
-        try
+        using (FileStream file = File.OpenRead(Constants.PATH_XML))
         {
-            using (FileStream file = File.OpenRead(Constants.PATH_XML))
-            {
-                questionsList = serializer.Deserialize(file) as List<QuestionAndAnswers>;
-            }
+            questionsList = serializer.Deserialize(file) as List<QuestionAndAnswers>;
+        }
 
-        }
-        catch (Exception e)
-        {
-            UIMethods.DisplayUI("There is no file available to load the questions from.\n");
-        }
         return questionsList;
     }
 
